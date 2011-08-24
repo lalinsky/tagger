378719315e4bc28dc44dfdb3045dea72e9f1c6c3
Lukáš Lalinský <lalinsky@gmail.com>
2011-08-24 15:53:34 +0200

    Support for custom tags

--- tagger.cpp
+++ tagger.cpp
@@ -16,6 +16,8 @@
 #include <id3v2tag.h>
 #include <id3v2frame.h>
 #include <id3v2header.h>
+#include <urllinkframe.h>
+#include <uniquefileidentifierframe.h>
 #include <textidentificationframe.h>
 #include <attachedpictureframe.h>
 #include <tfilestream.h>
@@ -24,7 +26,24 @@ using namespace std;
 using namespace TagLib;
 
 struct Metadata {
+	Metadata() :
+		hasArtist(0),
+		hasAlbum(0),
+		hasAlbumArtist(0),
+		hasTitle(0),
+		hasGenre(0),
+		hasTrackNumber(0),
+		hasTrackCount(0),
+		hasYear(0),
+		hasImage(0),
+		hasPublisher(0)
+	{}
 	String artist, album, albumartist, title, genre, publisher;
+	Map<String, String> customUserTextFrames;
+	Map<String, String> customTextFrames;
+	Map<String, String> customUserUrlFrames;
+	Map<String, String> customUrlFrames;
+	Map<String, String> customUniqueFileIdentifierFrames;
 	int trackNumber, trackCount, year;
 	ByteVector image;
 	int hasArtist : 1;
@@ -71,6 +90,72 @@ void setTextFrame(ID3v2::Tag *tag, const char *id, const String &text)
     ID3v2::TextIdentificationFrame *f = new ID3v2::TextIdentificationFrame(id, String::UTF16);
     f->setText(text);
 	tag->removeFrames(id);
+    tag->addFrame(f);
+}
+
+void setUserTextFrame(ID3v2::Tag *tag, const String &description, const String &text)
+{
+    ID3v2::UserTextIdentificationFrame *f = new ID3v2::UserTextIdentificationFrame(String::UTF16);
+    f->setDescription(description);
+    f->setText(text);
+    ID3v2::UserTextIdentificationFrame *of = ID3v2::UserTextIdentificationFrame::find(tag, description);
+	if (of) {
+		tag->removeFrame(of);
+	}
+    tag->addFrame(f);
+}
+
+void setUrlFrame(ID3v2::Tag *tag, const char *id, const String &text)
+{
+    ID3v2::UrlLinkFrame *f = new ID3v2::UrlLinkFrame(id);
+    f->setUrl(text);
+	tag->removeFrames(id);
+    tag->addFrame(f);
+}
+
+ID3v2::UserUrlLinkFrame *findUserUrlLinkFrame(ID3v2::Tag *tag, const String &description)
+{
+	ID3v2::FrameList l = tag->frameList("WXXX");
+	for (ID3v2::FrameList::Iterator it = l.begin(); it != l.end(); ++it) {
+		ID3v2::UserUrlLinkFrame *f = dynamic_cast<ID3v2::UserUrlLinkFrame *>(*it);
+		if (f && f->description() == description) {
+			return f;
+		}
+	}
+	return 0;
+}
+
+void setUserUrlFrame(ID3v2::Tag *tag, const String &description, const String &text)
+{
+    ID3v2::UserUrlLinkFrame *f = new ID3v2::UserUrlLinkFrame(String::UTF16);
+    f->setDescription(description);
+    f->setUrl(text);
+    ID3v2::UserUrlLinkFrame *of = findUserUrlLinkFrame(tag, description);
+	if (of) {
+		tag->removeFrame(of);
+	}
+    tag->addFrame(f);
+}
+
+ID3v2::UniqueFileIdentifierFrame *findUniqueFileIdentifierFrame(ID3v2::Tag *tag, const String &owner)
+{
+	ID3v2::FrameList l = tag->frameList("UFID");
+	for (ID3v2::FrameList::Iterator it = l.begin(); it != l.end(); ++it) {
+		ID3v2::UniqueFileIdentifierFrame *f = dynamic_cast<ID3v2::UniqueFileIdentifierFrame *>(*it);
+		if (f && f->owner() == owner) {
+			return f;
+		}
+	}
+	return 0;
+}
+
+void setUniqueFileIdentifierFrame(ID3v2::Tag *tag, const String &owner, const String &identifier)
+{
+    ID3v2::UniqueFileIdentifierFrame *f = new ID3v2::UniqueFileIdentifierFrame(owner, identifier.toCString(true));
+    ID3v2::UniqueFileIdentifierFrame *of = findUniqueFileIdentifierFrame(tag, owner);
+	if (of) {
+		tag->removeFrame(of);
+	}
     tag->addFrame(f);
 }
 
@@ -134,6 +219,21 @@ bool updateTags(char *path, Metadata *meta)
 		tag->removeFrames("APIC");
 		tag->addFrame(f);
 	}
+	for (Map<String, String>::Iterator it = meta->customTextFrames.begin(); it != meta->customTextFrames.end(); ++it) {
+		setTextFrame(tag, it->first.toCString(true), it->second);
+	}
+	for (Map<String, String>::Iterator it = meta->customUserTextFrames.begin(); it != meta->customUserTextFrames.end(); ++it) {
+		setUserTextFrame(tag, it->first, it->second);
+	}
+	for (Map<String, String>::Iterator it = meta->customUrlFrames.begin(); it != meta->customUrlFrames.end(); ++it) {
+		setUrlFrame(tag, it->first.toCString(true), it->second);
+	}
+	for (Map<String, String>::Iterator it = meta->customUserUrlFrames.begin(); it != meta->customUserUrlFrames.end(); ++it) {
+		setUserUrlFrame(tag, it->first, it->second);
+	}
+	for (Map<String, String>::Iterator it = meta->customUniqueFileIdentifierFrames.begin(); it != meta->customUniqueFileIdentifierFrames.end(); ++it) {
+		setUniqueFileIdentifierFrame(tag, it->first, it->second);
+	}
 	return file.save(MPEG::File::ID3v1 | MPEG::File::ID3v2, false, 3);
 }
 
@@ -141,7 +241,7 @@ int main(int argc, char **argv)
 {
 	int ch;
 	Metadata meta;
-	while ((ch = getopt(argc, argv, "a:b:A:t:n:N:G:Y:p:i:")) != -1) {
+	while ((ch = getopt(argc, argv, "a:b:A:t:n:N:G:Y:p:i:x:")) != -1) {
 		switch (ch) {
 		case 'a':
 			meta.artist = String(optarg, String::UTF8);
@@ -182,6 +282,47 @@ int main(int argc, char **argv)
 		case 'i':
 			meta.image = readFile(optarg);
 			meta.hasImage = 1;
+			break;
+		case 'x':
+			{
+				String tag(optarg, String::UTF8);
+				int offset = tag.find("=");
+				if (offset == -1) {
+					cerr << "invalid tag name " << tag.to8Bit() << endl;
+					break;
+				}
+				String name = tag.substr(0, offset).upper();
+				String value = tag.substr(offset + 1);
+				if (name.size() != 4) {
+					cerr << "invalid tag name " << tag.to8Bit() << endl;
+					break;
+				}
+				cerr << "parsing " << name.to8Bit() << " " << value.to8Bit() << endl;
+				if (name == "TXXX" || name == "WXXX" || name == "UFID") {
+					offset = value.find("=");
+					if (offset == -1) {
+						cerr << "invalid tag name " << tag.to8Bit() << endl;
+						break;
+					}
+					String description = value.substr(0, offset);
+					value = value.substr(offset + 1);
+					if (name == "TXXX") {
+						meta.customUserTextFrames[description] = value;
+					}
+					else if (name == "UFID") {
+						meta.customUniqueFileIdentifierFrames[description] = value;
+					}
+					else {
+						meta.customUserUrlFrames[description] = value;
+					}
+				}
+				else if (name[0] == 'T') {
+					meta.customTextFrames[name] = value;
+				}
+				else if (name[0] == 'W') {
+					meta.customUrlFrames[name] = value;
+				}
+			}
 			break;
 		case '?':
 		default:
